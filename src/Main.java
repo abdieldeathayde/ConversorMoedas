59aa472c83729881552f7b986ff8fdec9947b713
Abdiel Athayde <abdieldeathayde67@gmail.com>
2025-06-06 13:25:49 -0300

    feat: inicializando repositorio desafio

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+                ************************************************
+                Seja bem vindo/a ao Conversor de Moeda =]
+                
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Real brasileiro
+                4) Real brasileiro =>> Dólar
+                5) Dólar =>> peso colombiano
+                7) Sair
+                Escolha uma opção válida:
+                 ************************************************
+                """);
+
+        int opcao = sc.nextInt();
+
+    }
+
+    public void ConverteDolarParaPesoArgentino() {
+
+    }
+}
